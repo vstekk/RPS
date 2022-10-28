27327608d5674145ff0751830bb733db5df4b04f
vstekk <jakub.racek@gmail.com>
2022-10-28 11:54:51 +0200

    play game button

--- game.js
+++ game.js
@@ -1,31 +1,39 @@
-playGame();
+const gameDiv = document.getElementById('game');
+const newGameButton = document.createElement('div');
+newGameButton.classList.add("button");
+newGameButton.textContent = "Play Game";
+newGameButton.addEventListener('click', playGame);
+gameDiv.appendChild(newGameButton);
+
+
+function getPlayerChoice() {
+    //const input = prompt("Write rock, paper or scissors.");
+    const input = 'rock';
+    if (input === null) {
+        return "Clicked the stupid button!";
+    } else return input.toLowerCase();
+}
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        default:
+            return "scissors";
+    }
+}
 
 function playGame(){
+    newGameButton.remove();
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
     let playerScore = 0;
     let computerScore = 0;
 
-    function getPlayerChoice() {
-        const input = prompt("Write rock, paper or scissors.");
-        if (input === null) {
-            return "Clicked the stupid button!";
-        } else return input.toLowerCase();
-    }
-
-    function getComputerChoice() {
-        switch (Math.floor(Math.random() * 3)) {
-            case 0:
-                return "rock";
-            case 1:
-                return "paper";
-            default:
-                return "scissors";
-        }
-    }
     function playRound(playerSelection, computerSelection) {
-        alert(`Player: ${playerSelection}`);
-        alert(`Computer: ${computerSelection}`);
+        console.log(`Player: ${playerSelection}`);
+        console.log(`Computer: ${computerSelection}`);
 
         switch (playerSelection) {
             case "Clicked the stupid button!":
@@ -47,9 +55,9 @@ function playGame(){
     }
 
     for (let i = 1; i <= 5; i++){
-        alert(playRound(playerSelection, computerSelection));
-        alert("PLAYER " + playerScore + "  :  " + "COMPUTER" + computerScore)
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("PLAYER " + playerScore + "  :  " + "COMPUTER" + computerScore)
     }
 
-    alert("GAME ENDED")
+    console.log("GAME ENDED")
 }
